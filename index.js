9af9614da569db2864813671f4f0be99449f5621
Stephanie Phillips <steph@ladymacbeth.uk>
2018-10-23 18:35:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution here!
+// Write your solution here
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+  drivers.push("Ralph");
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift("Bob");
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+
+function appendDriver(name) {
+  const copyofdrivers = drivers.splice(2, 3, 'Steph');
+}
+
+function appendDriver(name) {
+  const copyofdrivers = drivers.splice();
+  copyofdrivers.
+}
+
+function prependDriver(name) {
+  const copyofdrivers2[] = drivers.splice(0, 1, 'Steph');
+}
+
+function removeLastDriver() {
+  const copyofdrivers3[] = drivers.slice(3);
+} 
+
+function removeFirstDriver() {
+  const copyofdrivers4[] = drivers.slice(0);
+}
+
+
